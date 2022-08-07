2d5427f3fabbcfc3e18c8d6267d522aff9c8bd96
tush-tr <tusharrajput604@gmail.com>
2022-08-07 11:31:34 +0530

    deploy infra

---
+++ terraform/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+  backend "s3" {
+    key = "aws/ec2-deploy/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+resource "aws_instance" "server" {
+  ami                    = "ami-052efd3df9dad4825"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.maingroup.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = var.private_key
+    timeout     = "4m"
+  }
+  tags = {
+    "name" = "DeployVM"
+  }
+}
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-LOGIN-AUTO"
+}
+resource "aws_security_group" "maingroup" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+
+
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+output "instance_public_ip" {
+  value     = aws_instance.server.public_ip
+  sensitive = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    default = "us-east-1"
+}
+variable "public_key" {
+  
+}
+variable "private_key" {
+  
+}
+variable "key_name" {
+  
+}
